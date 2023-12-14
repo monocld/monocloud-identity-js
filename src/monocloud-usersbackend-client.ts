ba65a53f0d99e9224b565ee93da4d5a3c8bd7243
Vishal Rastogi <maganuk@gmail.com>
2023-12-14 23:09:28 +0530

    Change Environment Variables

--- src/monocloud-usersbackend-client.ts
+++ src/monocloud-usersbackend-client.ts
@@ -11,13 +11,15 @@ export class MonoCloudUsersBackendClient extends UsersClient {
     options?: MonoCloudConfig,
     instance?: AxiosInstance
   ): MonoCloudUsersBackendClient {
-    const envTimeout = parseInt(process.env.MC_MANAGE_TIMEOUT ?? '', 10);
+    const envTimeout = parseInt(process.env.MC_USERS_BACKEND_TIMEOUT ?? '', 10);
 
     const opt: MonoCloudConfig = {
-      domain: options?.domain ?? process.env.MC_DOMAIN ?? '',
-      apiKey: options?.apiKey ?? process.env.MC_MANAGE_API_KEY ?? '',
+      domain: options?.domain ?? process.env.MC_USERS_BACKEND_DOMAIN ?? '',
+      apiKey: options?.apiKey ?? process.env.MC_USERS_BACKEND_API_KEY ?? '',
       config: options?.config ?? {
-        retry: options?.config?.retry ?? process.env.MC_MANAGE_RETRY === 'true',
+        retry:
+          options?.config?.retry ??
+          process.env.MC_USERS_BACKEND_RETRY === 'true',
         timeout:
           options?.config?.timeout ??
           (Number.isInteger(envTimeout) && envTimeout > 0)
