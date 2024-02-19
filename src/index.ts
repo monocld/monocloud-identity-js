03a1b16a4da667ed13d3441bc849c1f27649016e
Vishal Rastogi <maganuk@gmail.com>
2024-02-19 01:53:17 -0800

    Updated Pagination & Exception Handling

--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,41 @@
 import {
   MonoCloudConfig,
   MonoCloudResponse,
-  ModelStateError,
-  ModelStateException,
-  BadRequestException,
-  ForbiddenException,
   MonoCloudException,
-  NotFoundException,
-  ResourceExhaustedException,
-  ServerErrorException,
-  UnauthorizedException,
-  ConflictException,
+  MonoCloudBadRequestException,
+  MonoCloudConflictException,
+  MonoCloudErrorCodeValidationException,
+  MonoCloudForbiddenException,
+  MonoCloudKeyValidationException,
+  MonoCloudModelStateException,
+  MonoCloudNotFoundException,
+  MonoCloudRequestException,
+  MonoCloudResourceExhaustedException,
+  MonoCloudServerException,
+  MonoCloudUnauthorizedException,
+  ErrorCodeValidationError,
+  ErrorCodeValidationProblemDetails,
+  KeyValidationProblemDetails,
 } from '@monocloud/sdk-js-core';
 import { MonoCloudUsersBackendClient } from './monocloud-usersbackend-client';
 
 export { MonoCloudUsersBackendClient, MonoCloudResponse };
-export type { MonoCloudConfig, ModelStateError };
+export type { MonoCloudConfig, ErrorCodeValidationError };
 export {
-  BadRequestException,
-  ForbiddenException,
   MonoCloudException,
-  NotFoundException,
-  ResourceExhaustedException,
-  ServerErrorException,
-  UnauthorizedException,
-  ModelStateException,
-  ConflictException,
+  MonoCloudBadRequestException,
+  MonoCloudConflictException,
+  MonoCloudErrorCodeValidationException,
+  MonoCloudForbiddenException,
+  MonoCloudKeyValidationException,
+  MonoCloudModelStateException,
+  MonoCloudNotFoundException,
+  MonoCloudRequestException,
+  MonoCloudResourceExhaustedException,
+  MonoCloudServerException,
+  MonoCloudUnauthorizedException,
+  ErrorCodeValidationProblemDetails,
+  KeyValidationProblemDetails,
 };
 export * from './clients';
 export * from './models';
